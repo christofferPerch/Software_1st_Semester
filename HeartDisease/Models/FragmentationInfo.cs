88d9d3e6225e2e3832343f49f6c1d467b58df623
Ch0min <marklundgaard7@gmail.com>
2024-05-20 09:11:12 +0200

    all

--- HeartDisease/Models/FragmentationInfo.cs
+++ HeartDisease/Models/FragmentationInfo.cs
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
-﻿namespace HeartDisease.Models {
-    public class FragmentationInfo {
-        public string DatabaseName { get; set; }
-=======
-﻿namespace HeartDisease.Models
-{
-    public class IndexFragmentationHistory
-    {
+namespace HeartDisease.Models {
+    public class IndexFragmentationHistory {
         public int ID { get; set; }
->>>>>>> master
         public string TableName { get; set; }
         public string IndexName { get; set; }
         public float FragmentationPercent { get; set; }
