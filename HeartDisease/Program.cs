63b87739d40cc64e9c31b348d7c09c0dc1df64b8
christofferPerch <cph-cn332@cphbusiness.dk>
2024-05-24 10:02:39 +0200

    navbar

--- HeartDisease/Program.cs
+++ HeartDisease/Program.cs
@@ -69,7 +69,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Webshop}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 await EnsureRolesAsync(app);
